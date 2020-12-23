2a4a1478c204e0267e3b00c6e4de3e0b50d2ffbd
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2020-12-23 13:07:40 +0100

    Adding Folder wirth first linked list file(Creating and printing LinkedList)

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class LinkedLis
+    {
+
+        Node head;
+
+        public static class Node
+        {
+            int data;
+            Node next;
+
+
+            Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public static void printList()
+        {
+
+            Node n = head;
+            while (n != null)
+            {
+                Console.WriteLine(n.data + " ");
+                n = n.next;
+
+            }
+
+        }
+        public 
+            static void Main(string[] args)
+        {
+            LinkedLis firsLinkedList = new LinkedList();
+
+            firsLinkedList.head = new LinkedList.Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+
+            // Linking nodes 
+            firsLinkedList.head.next = second;
+            second.next = third;
+
+            firsLinkedList.printList();
+
+        }
+    }
+}
