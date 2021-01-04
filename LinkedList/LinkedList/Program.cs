9ec05c55256319b8a708814a99436104a5b2416b
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-01-04 19:52:44 +0100

    Making some changes into append methods in Linked List class, adding method insert After

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -42,28 +42,47 @@ namespace LinkedList
 
         }
 
-        public void insertAtFront(Node newHead)
+        public void insertAtFront(int data)
         {
+            Node newHead = new Node(data);
             Node tmp = head;
             head = newHead;
             head.next = tmp;
         }
 
-       public void insertAtEnd(Node newTail)
+       public void insertAtEnd(int data)
         {
+            Node newTail = new Node(data);
             Node tmp;
             tmp = head;
 
-            while (tmp != null)
+            while (tmp.next != null)
             {
                 tmp = tmp.next;
             }
 
             tmp.next = newTail;
-        } 
-        
-    
+            newTail.next = null;
+        }
 
+        public void insertAfter(int position, int data) {
+
+            Node After = new Node(data);
+            Node tmp;
+            Node previous;
+            previous = head;
+
+            previous = head;
+            
+            for(int i = 0; i < position-1; i++) {
+                previous = previous.next;
+            }
+
+            tmp = previous;
+            After.next = tmp.next;
+            previous.next = After;
+             
+        }
           public  static void Main(string[] args)
         {
             LinkedList firstLinkedList = new LinkedList();
@@ -71,14 +90,18 @@ namespace LinkedList
             firstLinkedList.head = new LinkedList.Node(1);
             Node second = new Node(2);
             Node third = new Node(3);
-            Node insertAtBeggining = new Node(10);
-            Node insertAtEnd = new Node(20);
+
+            int insertAtBeggining = 10;
+            int insertAtEnd = 20;
+            int insertAfterValue = 15;
+            int insertAfterNodeNumber = 2;
 
             firstLinkedList.head.next = second;
             second.next = third;
 
             firstLinkedList.insertAtFront(insertAtBeggining);
             firstLinkedList.insertAtEnd(insertAtEnd);
+            firstLinkedList.insertAfter(insertAfterNodeNumber, insertAfterValue);
             firstLinkedList.printList();
         }
     }
