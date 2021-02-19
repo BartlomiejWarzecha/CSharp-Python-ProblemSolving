c45f26be9346fc2399698c19b265127e044a0a9a
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-02-19 08:37:49 +0100

    Restoring files after missdealeting

---
+++ Array/Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Array
+{
+    class Array
+    {
+        
+        public static void printArray(int[] array) {
+
+            for (int i = 0; i < array.Length; ++i){
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static void mergeSort(int[] array) {
+
+        int length = array.Length;
+
+        int left = 0;
+        int endLeft = length / 2;
+        int right = endLeft + 1;
+
+
+        }
+
+        public void divide(int left, int right, int[] array) { 
+            
+            divide()
+            if()
+
+
+        }
+
+        static void Main(string[] args){
+            
+            int[] array =
+            { 1,2,3,4,5,6 };
+
+            printArray(array);
+        }
+    }
+}
---
+++ Objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Objects
+{
+    class Program
+    {
+        public class Point  
+        {
+            public int x;
+            public int y;
+
+            public void showPosition()
+            {
+                Console.WriteLine("Point x value is {0}, Point y value is {1}", x, y);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Point firstPoint = new Point();
+
+            firstPoint.x = 10;
+            firstPoint.y = 10;
+
+            firstPoint.showPosition();
+
+        }
+    }
+}
