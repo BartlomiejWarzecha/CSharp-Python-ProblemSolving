f950166acf3808a2bb8ff625e5400de35e4e3d0c
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-02-23 15:21:02 +0100

    Adding Bank account example class from C# Documentation

---
+++ Objects/BankAccountExample/BankAccount/BankAccount/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classes
+{
+    class Account
+    {
+        public static void main(string[] args) {
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
---
+++ Objects/BankAccountExample/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        private static int accountNumberSeed = 1234567890;
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
