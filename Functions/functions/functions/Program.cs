f7325184af53cc88cdd7d2f05d5714dcdcbc9b67
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-02-23 13:39:30 +0100

    Adding rough approac function isPalindrom

--- Functions/functions/functions/Program.cs
+++ Functions/functions/functions/Program.cs
@@ -31,22 +31,23 @@ namespace someFunctions
 
             int number;
             string userInput;
-            
 
-            while (minimum >= maximum) {
+
+            while (minimum >= maximum)
+            {
 
                 Console.WriteLine("Minimum value must be lower than maximum value, Please try again!");
 
-            Console.WriteLine("What is minimum number?");
-            userInput = Console.ReadLine();
-            minimum = Convert.ToInt32(userInput);
+                Console.WriteLine("What is minimum number?");
+                userInput = Console.ReadLine();
+                minimum = Convert.ToInt32(userInput);
 
-            Console.WriteLine("What is maximum number?");
-            userInput = Console.ReadLine();
-            maximum = Convert.ToInt32(userInput);
+                Console.WriteLine("What is maximum number?");
+                userInput = Console.ReadLine();
+                maximum = Convert.ToInt32(userInput);
 
 
-            
+
             }
 
             Console.WriteLine("Write a number between {0} and {1}(-1 to end)", minimum, maximum);
@@ -74,32 +75,33 @@ namespace someFunctions
         public static void printNumberOfVowelsAndDigits(string phrase = "2312", int amountOfDigits = 0, int amountOfVowels = 0)
         {
 
-            for (int i = 0; i < phrase.Length; i++) { 
+            for (int i = 0; i < phrase.Length; i++)
+            {
                 amountOfVowels += isVowel(phrase[i]);
                 amountOfDigits += isDigit(phrase[i]);
             }
 
             Console.WriteLine("Number of digits in string \" {0} \" = {1} " +
-                "Number of vowels = {2} ", phrase, amountOfDigits, amountOfVowels); 
+                "Number of vowels = {2} ", phrase, amountOfDigits, amountOfVowels);
         }
 
         public static int isVowel(char character)
         {
-                switch (Char.ToLower(character))
-                {
-                    case 'a':
-                    case 'o':
-                    case 'e':
-                    case 'i':
-                    case 'u':
-                    case 'y':
-                        return 1;
-                default: 
+            switch (Char.ToLower(character))
+            {
+                case 'a':
+                case 'o':
+                case 'e':
+                case 'i':
+                case 'u':
+                case 'y':
+                    return 1;
+                default:
                     return 0;
-                }
+            }
         }
 
-        public static int isDigit(char character) 
+        public static int isDigit(char character)
         {
             if (character >= 48 & character <= 57)
             {
@@ -125,52 +127,96 @@ namespace someFunctions
             
         }
 */
-        public static int howManyTimesInText(string phrase, string text) 
+        public static int howManyTimesInText(string phrase, string text)
         {
             int count = 0;
             int lenPhrase = phrase.Length;
             int lenText = text.Length;
+            int subString = 0;
 
-            for (int i = 0; i < lenText; i++) 
-            { 
+            for (int i = 0; i < lenText; i++)
+            {
 
-                    if (text[i] == phrase[0]) 
+                if (text[i] == phrase[0] | text[i] == phrase[subString])
+                {
+
+                    if (lenPhrase == 1)
                     {
-
-                    if (lenPhrase == 1) 
-                    { 
                         count += 1;
                         continue;
                     }
-                        for (int l = 1; l < lenPhrase;l++)
+
+                    for (int l = 1; l < lenPhrase; l++)
+                    {
+                        if (text[i + l] == phrase[l])
                         {
-                            if (text[i + l] == phrase[l])
+                            if (l + 1 == lenPhrase)
                             {
-                                if (l + 1 == lenPhrase)
-                                {
-                                    count += 1;
-                                    break;
-                                }
-                            } else
-                            {
+                                count += 1;
                                 break;
                             }
-
-                        } 
+                        }
+                        else
+                        {
+                            subString = l;
+                            break;
+                        }
                     }
+
+                    if (subString != 0)
+                    {
+                        for (int l = 1; l < lenPhrase - subString; l++)
+                        {
+                            if (text[i + l] == phrase[subString + l])
+                            {
+                                if (subString + 1 == lenPhrase)
+                                {
+                                    count += 1;
+                                    subString = 0;
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                }
             }
             return count;
         }
 
+        public static void isPalindrom(string text)
+        {
+            int halfStringLength = text.Length/2;
+            int stringLength = text.Length - 1;
+            Boolean isPalindrom = false;
+
+                for(int i = 0; i < halfStringLength; i++)
+                {
+                    if (text[i] == text[stringLength])
+                    {
+                        isPalindrom = true;
+                        stringLength--;
+                        continue;
+                    }
+                    isPalindrom = false;
+                    break;
+                }
+
+            if (isPalindrom)
+            {
+                Console.WriteLine($"This string {text} is Palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"This string {text} is not Palindrom");
+            }
+        }
 
         static void Main(string[] args)
         {
-            string randomPhrase = "l";
-            string randomText = "hello world hello c#";
+            string isPalindrome = "aa";
+            isPalindrom(isPalindrome);
 
-            int countPhraseReapeating = howManyTimesInText(randomPhrase, randomText);
-
-            Console.WriteLine($"String \"  {randomPhrase} \" is in text \" {randomText} \": {countPhraseReapeating} times ");
         }
 
     }
