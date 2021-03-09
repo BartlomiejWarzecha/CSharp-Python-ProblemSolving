67bc4fe3eda66e352839fd125e4bf69ebd7f76c4
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-09 08:20:11 +0100

    Adding Encryp and Decrypt class
    - It can be modify
     - How many numbers add in ecryption
     - Algoritm for encryption and decryption

---
+++ Classes/Encrypter/Enkrypter/Enkrypter/Encrypter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enkrypter
+{
+    class Encrypter
+    {
+        public static string Encrypt(string text)
+        {
+            int intLetter;
+            char encriptedLetter;
+            string encriptedText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                intLetter = (int)text[i] + 1;
+                encriptedLetter = (char)intLetter;
+                encriptedText += encriptedLetter.ToString();
+            }
+            return encriptedText;
+        }
+    
+
+        public static string Decrypt(string text)
+        {
+            int intLetter;
+            char decriptedLetter;
+            string decriptedText = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                intLetter = (int)text[i] - 1;
+                decriptedLetter = (char)intLetter;
+                decriptedText += decriptedLetter.ToString();
+            }
+            return decriptedText;
+        }
+    }
+}
---
+++ Classes/Encrypter/Enkrypter/Enkrypter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enkrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+
+            Console.WriteLine("Write text to encrypt");
+            text = Console.ReadLine();
+            Console.WriteLine($"Text for encription = {text}");
+            text = Encrypter.Encrypt(text);
+            Console.WriteLine($"Text after encription = {text}");
+            text = Encrypter.Decrypt(text);
+            Console.WriteLine($"Text after decription = {text}");
+        }
+    }
+}
