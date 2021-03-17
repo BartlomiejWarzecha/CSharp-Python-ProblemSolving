0b4d4b7685337bb688f0f0c493d2624c06d613d6
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-17 10:47:28 +0100

    Adding new Project with rough approach to number Compilator

---
+++ Others/numberCompilator/numberCompilator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace numberCompilator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string numberToCheck = "10,,1018120";
+            bool coma = false;
+
+
+            for (int i = 0; i < numberToCheck.Length; i++)
+            {
+                if (numberToCheck[0] == '0' && numberToCheck[1] == '0') 
+                {
+                    Console.WriteLine("Number cannot start with muliplied 0");
+                    break;
+                }
+
+                if (numberToCheck[i] == ',' && coma == true) 
+                {
+                    Console.WriteLine("Number cannot have muliplied ,");
+                    break;
+                }
+
+                if (numberToCheck[i] == ',') 
+                {
+                    coma = true;
+                    continue;
+                }
+            }
+
+        }
+    }
+}
