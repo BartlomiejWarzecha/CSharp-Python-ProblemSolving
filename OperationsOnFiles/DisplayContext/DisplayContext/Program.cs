79efce776243dd88f6355ce6fc6f12c277435765
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-17 09:07:40 +0100

    Adding function display file content

---
+++ OperationsOnFiles/DisplayContext/DisplayContext/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace DisplayContext
+{
+    class Program
+    {
+        public static void displayFileText(string fileName) 
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string nameOfFile;
+            if (args[0] != null)
+            {
+                nameOfFile = args[0];
+                Console.WriteLine($"Name of file = {nameOfFile} ");
+                Console.WriteLine($"Opening file = {nameOfFile} ");
+                displayFileText(nameOfFile);
+            }else{
+                Console.WriteLine("Write name of file you want to open, with its extension ");
+                nameOfFile = Console.ReadLine();
+                Console.WriteLine($"Opening file = {nameOfFile}");
+                displayFileText(nameOfFile);
+            }
+
+        }
+    }
+}
