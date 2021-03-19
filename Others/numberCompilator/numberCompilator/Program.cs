c79e03da0163fd522c869ece0999509e926af177
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-19 13:02:44 +0100

    Updating Program with
    - exit possibility(+)
    - Writing text after end
    - changing main loop with while instead of for

--- Others/numberCompilator/numberCompilator/Program.cs
+++ Others/numberCompilator/numberCompilator/Program.cs
@@ -7,30 +7,54 @@ namespace numberCompilator
         
         static void Main(string[] args)
         {
-            string numberToCheck = "10,,1018120";
+            string numberToCheck = "";
+            char digit;
             bool coma = false;
+            bool end = false;
+            int j = 0;
 
-
-            for (int i = 0; i < numberToCheck.Length; i++)
+            while(!end)
             {
-                if (numberToCheck[0] == '0' && numberToCheck[1] == '0') 
+                Console.WriteLine("Write one digit, if you want to end write + ");
+                digit = Console.ReadLine()[0];
+                
+                if (digit == '+')
                 {
-                    Console.WriteLine("Number cannot start with muliplied 0");
-                    break;
-                }
-
-                if (numberToCheck[i] == ',' && coma == true) 
-                {
-                    Console.WriteLine("Number cannot have muliplied ,");
-                    break;
-                }
-
-                if (numberToCheck[i] == ',') 
-                {
-                    coma = true;
+                    Console.WriteLine("Exiting Program");
+                    end = true;
                     continue;
                 }
+
+                numberToCheck += digit;
+                
+                if (numberToCheck[j] == ',' && coma == true) 
+                {
+                    Console.WriteLine("Number cannot have muliplied , try again");
+                    numberToCheck = "";
+                    j = 0;
+                    continue;
+                }
+
+                if (numberToCheck[j] == ',') 
+                {
+                    coma = true;
+                }
+                
+                if (numberToCheck.Length > 1)
+                {
+                    if (numberToCheck[0] == '0' && numberToCheck[1] == '0')
+                    {
+                        Console.WriteLine("Number cannot start with muliplied 0, try again");
+                        numberToCheck = "";
+                        j = 0;
+                        continue;
+                    }
+                }
+                j += 1;
+                
             }
+
+            Console.WriteLine($"You created number " + numberToCheck);
 
         }
     }
